5967d3a5681ee7045b8712af573af12a7f51ba24
harinivaskumar <harinivaskumar@live.com>
2016-12-23 03:26:21 +0530

    Code Commit
    Initial code commit

---
+++ app/src/main/java/com/hari/aag/actionbartabs/ActionBarTabActivityFragment.java
@@ -0,0 +1,22 @@
+package com.hari.aag.actionbartabs;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ActionBarTabActivityFragment extends Fragment {
+
+    public ActionBarTabActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_action_bar_tab, container, false);
+    }
+}
